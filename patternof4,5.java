09019d500a37f43e451540c8a8dc40880665b1b3
anvithachows <159905429+anvithachows@users.noreply.github.com>
2025-08-19 11:30:26 +0530

    Add files via upload

---
+++ patternof4,5.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=1; i<=8;i++){
+            if(i%2 == 0){
+            System.out.println();
+            continue;
+            }
+            System.out.println(" * * * * * ");
+        }
+    }
+}
