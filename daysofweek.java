9e79cf21924322c5ffcc99ce703d8f9bf6244a83
anvithachows <159905429+anvithachows@users.noreply.github.com>
2025-08-19 12:31:35 +0530

    Add files via upload

---
+++ daysofweek.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println("Days of the week: ");
+        for(int i = 0; i < days.length; i++){
+            System.out.println(days[i]);
+        }
+    }
+}
