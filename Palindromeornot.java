88f2a82ec9eb6f8eafd21846f0403e1cbbb2b28f
anvithachows <159905429+anvithachows@users.noreply.github.com>
2025-08-19 12:04:17 +0530

    Add files via upload

---
+++ Palindromeornot.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void main(String[] args){
+        int num = 121;
+        int temp = num;
+        int rev = 0;
+        while(temp > 0){
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp = temp / 10;
+        }
+        if(num == rev){
+            System.out.println(num + " is a Palindrome");
+        }
+        else{
+            System.out.println(num + " is not a Palindrome");
+        }
+    }
+}
