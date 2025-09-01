8acde85211d0f27acd77010b09eeb1c86ad66a37
anvithachows <159905429+anvithachows@users.noreply.github.com>
2025-09-01 16:08:51 +0530

    Add files via upload

---
+++ PrimeNumbers1to100.java
@@ -0,0 +1,17 @@
+class PrimeNumbers1to100 {
+    public static void main(String [] args){
+        System.out.println("Prime number between 1 and 100 are:");
+        for(int num = 2; num <= 100 ; num++ ){
+            boolean isPrime = true;
+            for(int i = 2; i <= Math.sqrt(num); i++){
+                if(num % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.println(num +" ");
+            }
+        }
+    }
+}
